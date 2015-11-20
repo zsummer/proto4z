b9041ceb2f2b5886ee40732dedbb7c1b699fcca0
zsummer <yawei.zhang@foxmail.com>
2015-11-20 16:15:43 +0800

    update

--- test/travis/setup.sh
+++ test/travis/setup.sh
@@ -2,8 +2,9 @@
 if [ "${TRAVIS_OS_NAME}" = "osx" ]; then
   echo "osx system" 
 else
-  apt-get install cmake
-  apt-get install -qq g++-4.8
+#  apt-get install cmake
+#  apt-get install -qq g++-4.8
   export CXX="g++-4.8"
+#  sudo ifconfig
 fi
 
