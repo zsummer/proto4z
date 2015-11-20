e245d092728047e7d6d52bfbb24ce54e221d2363
zsummer <yawei.zhang@foxmail.com>
2015-11-20 16:24:01 +0800

    update

--- test/travis/env.sh
+++ test/travis/env.sh
@@ -4,8 +4,5 @@ if [ "${TRAVIS_OS_NAME}" = "osx" ]; then
 else
   add-apt-repository -y ppa:ubuntu-toolchain-r/test
   apt-get update -qq
-  apt-get install cmake
-  apt-get install -qq g++-4.8
-  export CXX="g++-4.8"
 fi
 
