a5c296d589852b64e4b8c3ea7f2564d97a19bd9d
zsummer <yawei.zhang@foxmail.com>
2015-11-20 16:56:01 +0800

    update

--- test/travis/env.sh
+++ test/travis/env.sh
@@ -3,7 +3,7 @@ if [ "${TRAVIS_OS_NAME}" = "osx" ]; then
   echo "osx system" 
 else
   echo "linux system" `whoami`
-  sudo --user `whoami` add-apt-repository -y ppa:ubuntu-toolchain-r/test 
-  sudo --user `whoami` apt-get update -qq
+  sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test 
+  sudo apt-get update -qq
 fi
 
--- test/travis/setup.sh
+++ test/travis/setup.sh
@@ -3,8 +3,8 @@ if [ "${TRAVIS_OS_NAME}" = "osx" ]; then
   echo "osx system" 
 else
   echo "linux system" `whoami`
-  sudo --user `whoami` apt-get install -qq cmake
-  sudo --user `whoami` apt-get install -qq g++-4.8
+  sudo apt-get install -qq cmake
+  sudo apt-get install -qq g++-4.8
   export CXX="g++-4.8"
 fi
 
